dfd09f797ebb55052b32f24686429dee0f6068a3
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2020-10-18 15:25:39 -0500

    Initial Commit

---
+++ TestiOSSnapshot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestiOSSnapshot
+//
+//  Created by Miguel Mexicano Herrera on 18/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
